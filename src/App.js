7d89b28b811e351e8906e0c5e4b41220bb35389a
mariana <maritaborue@gmail.com>
2021-10-02 21:39:12 -0500

    "changes2"

--- src/App.js
+++ src/App.js
@@ -4,25 +4,42 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Tonces texto
-        </p>
-        <img src={'https://cdn.pixabay.com/photo/2017/12/31/13/49/barn-owl-3052382_1280.jpg'} />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-          
-        >
-          Learn React
-        </a>
-      </header>
+      <header>
+      <h1>This is a test to learn uploading pages in Github</h1>
+    </header>
+    <nav>
+      <a href="#">Home</a>
+      <a href="#">Photo Gallery</a>
+      <a href="#">Contact</a>
+      <a href="#">About me</a>
+    </nav>
 
-      
+    <main>
+      <div class="row">
+        <aside>
+          <h2>Aside</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem magnam placeat hic vitae minus natus
+            laudantium dicta adipisci impedit, quis, neque fugiat, in possimus accusantium dignissimos aliquam a est
+            ratione!
+          </p>
+        </aside>
+        <section>
+          <h2>Section</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem magnam placeat hic vitae minus natus
+            laudantium dicta adipisci impedit, quis, neque fugiat, in possimus accusantium dignissimos aliquam a est
+            ratione!
+          </p>
+        </section>
+      </div>
+    </main>
 
+    <footer>
+      <h2>Footer</h2>
+    </footer>
+        
+  
     </div>
   );
 }
